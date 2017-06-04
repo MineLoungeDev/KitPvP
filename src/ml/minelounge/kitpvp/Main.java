719acda2e57fd468210195cc4774223b3c9eacd3
RobinMC <robinslot@hotmail.nl>
2017-06-04 16:47:23 +0200

    Plugin variabele toegevoegd

--- src/ml/minelounge/kitpvp/Main.java
+++ src/ml/minelounge/kitpvp/Main.java
@@ -3,5 +3,12 @@ package ml.minelounge.kitpvp;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+	
+	public static Main plugin;
+	
+	@Override
+	public void onEnable(){
+		plugin = this;
+	}
 
 }
